86b523d3acb7926181a353e20ecaca053d97a1b6
aggarabedian <abrahamgarabedian@gmail.com>
2021-07-14 16:45:11 -0700

    Timer working.

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,9 @@ A new set of 3 buttons will appear, as will a matching meter bar for each one: H
 Each button will have a name or image to represent it; food(donut), play(ball), sleep(Zzzz).
 An 'age' will display at the top, next to the blob's name -- it will be 0 to start.  This will go up 1 year every 30 seconds.
 Meanwhile, the 3 meters will all fill up slowly, and the user must click the corresponding button to keep it from filling up.
-At year 3, the blob grows into an adolescent, gainging feet and short arms. The meters also begin to speed up.
-At year 5, the blob grows into an adult, getting bigger and an antenna on it's head.
-At year 6, a UFO will appear and beam the blob up, ending the game.
+At year 1, the blob grows into an adolescent, gainging feet and short arms. The meters also begin to speed up.
+At year 3, the blob grows into an adult, getting bigger and an antenna on it's head.
+At year 4, a UFO will appear and beam the blob up, ending the game.
 "The truth is out there." appears, as does a 'new game' button.
 If the user fails to keep any meter from filling up all the way, the blob will get a sad face and shrink to nothing; "Your blob has melted away -- better luck next time!"; 'new game' button appears.
 */
@@ -64,16 +64,27 @@ const blob = {
   },
 
   startTimer() {
-    blob.time = setInterval(blob.overTime, 1000);
+    blob.timer = setInterval(blob.overTime, 1000);
   },
 
   overTime() {
     blob.time--;
-    $(".gameClock").text(`Time left this year: ${blob.time}s`);
+    $(".game-clock").text(`Time left this year: ${blob.time}s`);
     if (blob.time <= 0) {
       blob.age++;
+      blob.setUpRound();
     }
   },
+
+  setUpRound() {
+    clearInterval(blob.timer);
+    if (blob.age === 5) {
+      //game over
+      console.warn("Game Over");
+    }
+  },
+
+
 
   feedBlob(event) {
     if (blob.hunger < 10 && blob.hunger > 0) {
